7e2041532d088be7b65319b17ec19cca81e30f2f
Alok Kumar <alokjkumar@outlook.com>
2019-09-11 11:13:54 +0100

    Generated FormList Page

--- src/EnabledBanking/src/app/app-routing.module.ts
+++ src/EnabledBanking/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'main', loadChildren: './main/main.module#MainPageModule' },
+  { path: 'form-list', loadChildren: './form-list/form-list.module#FormListPageModule' },
 ];
 
 @NgModule({
