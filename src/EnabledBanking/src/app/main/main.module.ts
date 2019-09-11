8705b8f8cd5891c36cc98566cc1b39b1c0548aa7
Alok Kumar <alokjkumar@outlook.com>
2019-09-11 11:11:06 +0100

    Main Page Design

--- src/EnabledBanking/src/app/main/main.module.ts
+++ src/EnabledBanking/src/app/main/main.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     AccountSummaryComponentModule,
+    VoiceControlBtnComponentModule,
     RouterModule.forChild(routes)
   ],
   declarations: [MainPage]
