7c17c2a79a8bb41a9864b13291debb0e9d068d9f
Alok Kumar <alokjkumar@outlook.com>
2019-09-11 11:11:00 +0100

    Voice Control Button

--- src/EnabledBanking/src/app/voice-control-btn/voice-control-btn.component.ts
+++ src/EnabledBanking/src/app/voice-control-btn/voice-control-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-voice-control-btn',
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoiceControlBtnComponent implements OnInit {
 
+    color = null;
+    fill = null;
+    @Input() properties:Array = ["primary", true];
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(this.properties[1]) {
+      this.fill = "solid";
+    } else {
+      this.fill = "outline";
+    }
+    this.color = this.properties[0];
+  }
 
 }
