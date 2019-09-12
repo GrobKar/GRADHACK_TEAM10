170ee1b53907025a460bcf6be33b036f96c3234c
Alok Kumar <alokjkumar@outlook.com>
2019-09-12 06:56:27 +0100

    Added navigation to form items

--- src/EnabledBanking/src/app/form-list-item/form-list-item.module.ts
+++ src/EnabledBanking/src/app/form-list-item/form-list-item.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FormListItemComponent } from './form-list-item.component';
 
@@ -8,6 +9,7 @@ import { FormListItemComponent } from './form-list-item.component';
   imports: [
     CommonModule,
     IonicModule,
+    RouterModule
   ],
   declarations: [FormListItemComponent],
   exports: [FormListItemComponent]
