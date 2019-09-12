34d2f638a4ee4a29d17da15acc4b20833be86707
Alok Kumar <alokjkumar@outlook.com>
2019-09-12 06:50:50 +0100

    Built and designed handwriting component

--- src/EnabledBanking/src/app/handwriting/handwriting.component.ts
+++ src/EnabledBanking/src/app/handwriting/handwriting.component.ts
@@ -1,14 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-handwriting',
   templateUrl: './handwriting.component.html',
   styleUrls: ['./handwriting.component.scss'],
 })
-export class HandwritingComponent implements OnInit {
+export class HandwritingComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('handwritingCanvas', {static: false}) canvas: any;
+  canvasElement: any;
 
-  ngOnInit() {}
+  saveX: number;
+  saveY: number;
+
+  constructor(private plt: Platform) { }
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasElement.width = this.plt.width();
+    this.canvasElement.height = 325;
+  }
+
+  startDrawing(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+
+    this.saveX = ev.touches[0].pageX - canvasPosition.x;
+    this.saveY = ev.touches[0].pageY - canvasPosition.y;
+  }
+
+  moved(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+
+    let ctx = this.canvasElement.getContext('2d');
+    let currentX = ev.touches[0].pageX - canvasPosition.x;
+    let currentY = ev.touches[0].pageY - canvasPosition.y;
+
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 5;
+
+    ctx.beginPath();
+    ctx.moveTo(this.saveX, this.saveY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+
+    ctx.stroke();
+
+    this.saveX = currentX;
+    this.saveY = currentY;
+  }
+
+  public clear() {
+    let ctx = this.canvasElement.getContext('2d');
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  }
+  public exportCanvasImage() {
+    let dataUrl = this.canvasElement.toDataURL();
+    let data = dataUrl.split(',')[1];
+    let blob = this.b64toBlob(data, 'image/png');
+    this.clear();
+
+    return blob;
+  }
+
+  b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      var byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
 
 }
